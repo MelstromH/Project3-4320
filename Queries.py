24cd54f24c78c458ccd7b1d70b58a067da2406dc
Will Spencer <wspencer2000@gmail.com>
2020-10-27 21:34:05 -0500

    Added error checking for input 2 and 3

--- Queries.py
+++ Queries.py
@@ -1,26 +1,32 @@
 #Function one to get input for the stock symbol. 
 userInput1 = input("Enter the stock symbol you are looking for:")
 
-print("Your Choices are: Bar and Chart")
+print("Your Choices are: \n1.Bar \n2.Chart")
 
 #Function two to get input on what chart they want.
-userInput2 = input("Enter the type of chart you want:")
+userInput2 = int(input("Enter the type of chart you want:"))
 
-#If Else Loop for the Input
-if (userInput2 == "Bar"):
-    print("cool")
+while(True):
+    if (userInput2 == 1):
+        break   
     
-
-elif (userInput2 == "Line"):
-    print("Very cool")
+    elif (userInput2 == 2):
+        break
     
-
-else:
-    print("That is not a valid input your choices are Bar or Line. Please try again.")
+    else:
+        print("\nThat is not a valid input your choices are 1.Bar or Line. Please try again.\n")
+        userInput2 = int(input("Enter the type of chart you want:"))
     
-
-print("Time Series Selections: 1.Intraday 2.Daily 3.Weekly 4.Monthly")
-
+print("\nTime Series Selections: \n1.Intraday \n2.Daily \n3.Weekly \n4.Monthly\n")
 
 #Function Three to get input for the Time Series selection.
-userInput3 = input("Enter the time series option you'd like:") 
+userInput3 = int(input("Enter the time series option you'd like:")) 
+
+while(True):
+    if (userInput3 > 0 & userInput3 < 5):
+        break   
+    
+    else:
+        print("\nThat is not a valid input your choices are \n1.Intraday \n2.Daily \n3.Weekly \n4.Monthly\n")
+        userInput3 = int(input("Enter the time series option you'd like:"))
+    
