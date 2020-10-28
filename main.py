f34974fbdb637d6788bddc53c653cfb42227a1a9
MelstromH <32555061+MelstromH@users.noreply.github.com>
2020-10-28 18:38:37 -0500

    Added Date Func

--- main.py
+++ main.py
@@ -3,8 +3,48 @@ from Queries import *
 
 userChoices = queries()
 
+while True:
+    # Collect dates from user and split into three different fields
+    try:
+        begin_date = input('Enter a beginning date in YYYY-MM-DD format: ')
+        begin_year, begin_month, begin_day = map(int, begin_date.split('-'))
+        date1 = datetime.date(begin_year, begin_month, begin_day)
+
+        end_date = input('Enter a end date in YYYY-MM-DD format: ')
+        end_year, end_month, end_day = map(int, end_date.split('-'))
+        date2 = datetime.date(end_year, end_month, end_day)
+
+        # Compare the dates to make sure they are valid
+        if (begin_year > end_year):
+            print("Enter valid dates")
+            continue
+        elif (begin_year == end_year):
+            if (begin_month > end_month):
+                print("Enter valid dates")
+                continue
+            elif (end_month > begin_month):
+                break
+            elif (end_month == begin_month):
+                if (begin_day >= end_day):
+                    print("Enter valid dates")
+                    continue
+                else:
+                    break
+        else:
+            break
+
+    # Using Try/Except to easily restart if an error occurs
+    except:
+        print("Enter valid dates!")
+        continue
+
+# Prints out the dates to make sure everything worked the way it's supposed to
+print("Start date: " + str(date1))
+print("End date: " + str(date2))
+
+
 if userChoices[1] == 1:
-    printBarGraph(userChoices[0], userChoices[2])
+    printBarGraph(userChoices[0], date1, date2, 1, userChoices[2])
 else:
-    printLineGraph(userChoices[0], userChoices[2])
+    printLineGraph(userChoices[0], date1, date2, 1, userChoices[2])
 
